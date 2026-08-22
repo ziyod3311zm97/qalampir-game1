2605280a03c145e0beaa2f379297302e5f8bc8e3
ziyod3311zm97 <ziyod3311zm97@gmail.com>
2026-08-22 18:39:24 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// index.html joylashuvini tekshirish (ildizda yoki public papkasida)
+app.get('/', (req, res) => {
+    const rootPath = path.join(__dirname, 'index.html');
+    const publicPath = path.join(__dirname, 'public', 'index.html');
+
+    if (fs.existsSync(rootPath)) {
+        res.sendFile(rootPath);
+    } else if (fs.existsSync(publicPath)) {
+        res.sendFile(publicPath);
+    } else {
+        res.status(404).send('index.html fayli topilmadi!');
+    }
+});
+
+// Statik fayllarni ulash
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const usersDB = {};
